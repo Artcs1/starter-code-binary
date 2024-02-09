201e67c370f219901ca06f1b2485ec2d9f078100
Artcs <ju.jeffri.v@gmail.com>
2024-02-09 06:33:49 -0500

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,20 @@
+from math import *
+
+def function(p, q, r, s, t, u, X):
+# TODO
+
+def bisection(ini, fin):
+# TODO
+
+while True:
+    try:
+        line = input()
+        p, q, r ,s, t, u = map(float, line.split(' '))
+        if : # TODO
+            print('No solution')
+        else:
+            ini = 0.0
+            fin = 1.0
+            print('%.4f' % bisection(ini, fin))
+    except(EOFError):
+        break
